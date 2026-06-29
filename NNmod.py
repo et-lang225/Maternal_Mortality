f7346e47227b2e9b32a8f547258e4f904faf123c
et-lang225 <et.lang225@gmail.com>
2026-06-29 06:36:09 -0500

    data updates

--- NNmod.py
+++ NNmod.py
@@ -1,24 +1,39 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.metrics import average_precision_score
 
 class MomMortMod(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size, hidden_layers):
         super().__init__()
-        self.fc1 = nn.Linear(10, 5)
-        self.fc2 = nn.Linear(5, 1)
+        self.mid_layers = nn.ModuleList()
+        current_dim = input_size
+        
+        for hidden_dim in hidden_layers:
+            self.mid_layers.append(nn.Linear(current_dim, hidden_dim))
+            current_dim = hidden_dim
+        
+        self.output_layer = nn.Linear(current_dim, 1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
     
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.sigmoid(self.fc2(x))
-        return x
+        for layer in self.mid_layers:
+            x = self.relu(layer(x))
+        x = self.output_layer(x)
+        return self.sigmoid(x)
 
 class NN_mod:
-    def __init__(self, epochs=100, lr=0.01):
-        self.model = MomMortMod()
-        self.criterion = nn.BCELoss()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)                                   
+    def __init__(self, input_size, hidden_layers, epochs=100, lr=0.01):
+        self.input_size = input_size
+        self.hidden_layers = hidden_layers
+        self.lr = lr                                
         self.epochs = epochs
+        
+        self.criterion = nn.BCELoss()
+        self.model = None
+        self.optimizer = None
 
     def fit(self, X_train, y_train, X_valid, y_valid):
         X_train = torch.from_numpy(X_train).float()
@@ -28,17 +43,49 @@ class NN_mod:
         train_log = []
         valid_log = []
         for epoch in range(self.epochs):
+            self.model.train()
             self.optimizer.zero_grad()
             pred_train = self.model(X_train)
             loss = self.criterion(pred_train, y_train)
-            pred_valid = self.model(X_valid)
-            valid_loss = self.criterion(pred_valid, y_valid)
             loss.backward()
             self.optimizer.step()
             
-            if (epoch+1) % 5 == 0:
-                print(f"Epoch: {epoch}, Loss: {loss.item():.4f}")
+            self.model.eval()
+            with torch.no_grad():
+                pred_valid = self.model(X_valid)
+                valid_loss = self.criterion(pred_valid, y_valid)
+            
+            if (epoch+1) % 25 == 0:
+                print(f"  Epoch: {epoch + 1:3d} | Train Loss: {loss.item():.4f} | Valid Loss: {valid_loss.item():.4f}")
             train_log.append(loss.item())
             valid_log.append(valid_loss.item())
+        return train_log, valid_log
+    
+    def NN_CV(X, y, k=5,):
+        kf = KFold(n_splits=k, shuffle=True, random_state=42)
+        fold_results = []
+        
+        for fold, (train_ids, val_ids) in enumerate(kf.split(x)):
+            print(f"Fold: {fold+1} of {k}")
+            
+            self.model = MomMortMod(self.input_size, self.hidden_layers)
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+            
+            X_train = X[train_ids,:]
+            X_val = X[val_ids,:]
+            y_train = y[train_ids,:]
+            y_val = y[val_ids,:]
+            
+            t_log, v_log =  self.fit(X_train, y_train, X_val, y_val)
+            
+            self.model.eval()
+            with torch.no_grad():
+                val_tensor = torch.from_numpy(X_val).float()
+                predictions = self.model(val_tensor)
+                avg_precision = average_precision_score(y_val, predictions)
+                print(f"Fold {fold + 1} Final Mean Precision: {avg_precision:.2f}%")
+                fold_results.append(avg_precision)
+        print(f"\n=== CV Average Accuracy: {np.mean(fold_results):.2f}% ===")
+        return fold_results
             
             
--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Maternal Mortality
 #### I will be using Synthea data to predict maternal mortality from a number of different factors. This README file will be updated as I make progress.
-#### So far I have only started simulating data and converting csv files to a SQL database.
 #### The processing of the database files could be completed entirely within pandas, but I wanted to simulate a traditional database extraction in order to present an example pipeline.
-#### I am working on getting enough data (1million records) to split data into a train and test set
-#### I queried 1,000 total patients first but that only yeilded about 200 pregnant women and 5 maternal mortality cases. I will have to be vigilant about model evaluation metrics to be sure that True Positives are being classified correctly (possibly focusing on Precision and Recall). 
+#### The most feasible way to do this right now with the processing power available to me is to query 100K patients from Synthea
 #### So far have code to run a Naive Bayes classifier, a Logisitic Regression Cross Validation (for EN regularization hyperparameters), A Gaussian SVM classifer, a cross-validation loop for a Random Forest classifier, a cross-validation loop for a gradient boosting classifier, and the bones of a PyTorch Neural Network classifier
 #### Need to include a cross validation loop for the NN model 
 
@@ -13,10 +11,10 @@
 git clone https://github.com/synthetichealth/synthea.git         
 cd synthea         
 .\gradlew.bat build check test        
-.\run_synthea -p 1000000 -g F "Louisiana"
+.\run_synthea -p 100000 -g F "Louisiana"
 ```         
 
-Note: Might have to install the most recent version of java in order to run synthea. I had to install a local java folder and inform the powershell to run java from that folder.
+Note: Might have to install the most recent version of java in order to run synthea. I used a network java folder and ran java from that folder.
 ```bash          
 Invoke-WebRequest -Uri "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.11%2B9/OpenJDK17U-jdk_x64_windows_hotspot_17.0.11_9.zip" -OutFile "jdk17.zip"         
 Expand-Archive -Path "jdk17.zip" -DestinationPath ".\jdk17"         
