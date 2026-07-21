6ddb20860ab48225cedf46028e39b7808b4eed94
et-lang225 <et.lang225@gmail.com>
2026-07-21 07:15:45 -0500

    fix SVM and RF

--- RandForest.py
+++ RandForest.py
@@ -6,18 +6,17 @@ class RF_hyperparams:
     def __init__(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
-    def best_fit(self):
+        self.rf = RandomForestClassifier(random_state=42)
+    def SearchCV(self):
         param_grid = {
         'n_estimators': np.arange(100,1000,100),
         'max_depth': np.arange(5,50,5),
         'min_samples_split': [2,4,6],
         'min_samples_leaf': [1,2,4,6]
         }
-        rf = RandomForestClassifier(random_state=42)
-        RF_grid_search = RandomizedSearchCV(rf, param_distributions=param_grid, n_iter=20, scoring='neg_log_loss', n_jobs=8, cv=5, verbose=2)
-        RF_grid_search.fit(self.X_train, self.y_train)
-        best_rf_model = RF_grid_search.best_estimator_
-        return {'best': best_rf_model, 'model': RF_grid_search}
-    def predict(self, X_test):
-        best_model = self.best_fit()['best']
-        return best_model.predict(X_test)
+        return RandomizedSearchCV(self.rf, param_distributions=param_grid, n_iter=10, scoring='neg_log_loss', n_jobs=8, cv=5, verbose=2)
+    def best_fit(self, search_obj):
+        search_obj.fit(self.X_train, self.y_train)
+        return print(search_obj.best_estimator_)
+    def predict(self, X_test, search_obj):
+        return search_obj.predict(X_test)
